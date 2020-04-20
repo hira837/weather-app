a66aa0c1054145cf9367f5066e4bbefb6b07b327
Yuki Hirayama <gfyxm868@gmail.com>
2020-04-21 00:23:04 +0900

    modify text geocode.js

--- utils/geocode.js
+++ utils/geocode.js
@@ -8,17 +8,17 @@ const geocode = (address, callback) => {
 
   request({ url: url, json: true }, (error, response) => {
     if (error) {
-      callback("Unable to connect to location service!", undefined);
-    } else if (!response.body.features.length) {
-      callback("Unable to find loaction. Try another search", undefined);
+      callback("Unable to connect to location service!", undefined)
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find loaction. Try another search", undefined)
     } else {
       callback(undefined, {
-        longtitude: response.body.features[0].center[0],
         latitude: response.body.features[0].center[1],
+        longitude: response.body.features[0].center[0],
         location: response.body.features[0].place_name,
-      });
+      })
     }
-  });
-};
+  })
+}
 
 module.exports = geocode
