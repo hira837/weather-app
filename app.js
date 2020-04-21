ef75f01fb0b8c18da6d03831822139facc1f242c
Yuki Hirayama <gfyxm868@gmail.com>
2020-04-21 14:19:34 +0900

    implement location setting from command

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode("Boston", (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
-  forecast(data.latitude, data.longitude, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+const locationFromCommand = process.argv[2]
+
+if(!locationFromCommand) {
+  console.log('Please provide location name on command')
+} else {
+  geocode(locationFromCommand, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
   })
-});
+}
